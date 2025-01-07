521e3cf6d4a7136f1883d82e8974989ad6d51d04
rezaderakhshan <rezaderakhshanich@gmail.com>
2025-01-07 06:48:20 +0330

    add font inter to test b

--- src/app/testB/page.tsx
+++ src/app/testB/page.tsx
@@ -1,8 +1,12 @@
 import FormTestB from "@/containers/test-b/form-test-b";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"], display: "swap" });
 
 const Page = () => {
   return (
-    <div className="mb-[136px] mt-[57px] flex justify-center">
+    <div
+      className={`${inter.className} mb-[136px] mt-[57px] flex justify-center`}
+    >
       <FormTestB />
     </div>
   );
